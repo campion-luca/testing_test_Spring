c33acfce00726115279e1343d1ea32558edfc002
campion-luca <campionluca3@gmail.com>
2024-10-23 17:41:20 +0200

    add drink test

--- src/test/java/com/epicode/U5D2/Esercizio30102023ApplicationTests.java
+++ src/test/java/com/epicode/U5D2/Esercizio30102023ApplicationTests.java
@@ -4,15 +4,19 @@ import com.epicode.U5D2.entities.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class Esercizio30102023ApplicationTests {
 
 	@Test
-	void testIfReturnAPizza() {
+	void possiamoCreareDrink() {
 		System.out.println("-------------TEST 1-------------");
-
+		Drink bianchi = new Drink("Margherita test",100,20.0);
+		assertNotNull(bianchi);
 	}
+
+
 
 }
