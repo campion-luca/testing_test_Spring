f0ac0bec2cef1528ec9eb1f7f5147c6496bf8f53
campion-luca <campionluca3@gmail.com>
2024-10-23 16:45:05 +0200

    Initial commit

---
+++ src/test/java/com/epicode/U5D2/Esercizio30102023ApplicationTests.java
@@ -0,0 +1,18 @@
+package com.epicode.U5D2;
+
+import com.epicode.U5D2.entities.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class Esercizio30102023ApplicationTests {
+
+	@Test
+	void testIfReturnAPizza() {
+		System.out.println("-------------TEST 1-------------");
+
+	}
+
+}
